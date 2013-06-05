4eeca63ce5ee0fcf97f927b69b77cdd50a23ca8e
Vakulenko Michael <vakulenkom@gmail.com>
2013-06-05 19:40:28 +0400

    historyView: table

---
+++ Alco/fonts/RegularLabel.h
@@ -0,0 +1,13 @@
+//
+//  BoldCondLabel.h
+//  Alco
+//
+//  Created by Михаил Вакуленко on 01.06.13.
+//  Copyright (c) 2013 peacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegularLabel : UILabel
+
+@end
